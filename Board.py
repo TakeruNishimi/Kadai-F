d31638bb9b38290ce84f52dc66a9125f5c6207b1
TakeruNishimi <takechu.nishimi@gmail.com>
2019-09-26 11:37:01 +0900

    アホフィルター アホ、あほ、阿呆の文字列を名前かメッセージに使うと <桂三度>に置換される。

--- Board.py
+++ Board.py
@@ -1,7 +1,34 @@
-from flask import Flask, render_template, request ,redirect
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+
+class WordFilter:
+    def __init__(self, NG_word_list, replace_word):
+        self.NG_word_list = NG_word_list
+        self.replace_word = replace_word
+
+    def censor(self, sentence):
+        replace_sentence = sentence
+        for NG_word in self.NG_word_list:
+            replace_sentence = replace_sentence.replace(NG_word, self.replace_word)
+        return replace_sentence
+
+
+def make_NG_list():
+    NG_word_list = []
+    while True:
+        print('NGワードを入力してください。')
+        NG_word = input("これ以上必要ないときはそのままエンターを押してください。　＞　")
+        if NG_word == '':
+            break
+        NG_word_list.append(NG_word)
+    return NG_word_list
+
+
+NG_word_list = ['アホ', 'あほ', '阿呆']
+replace_word = '<桂三度>'
+my_filter = WordFilter(NG_word_list=NG_word_list, replace_word=replace_word)
 message_list = []
 user_list = []
 
@@ -12,9 +39,11 @@ def form():
         return render_template("board.html", message_list=message_list)
     if request.method == 'POST':
         username = request.form['username']
-        user_count = user_list.count(username) +1
+        username = my_filter.censor(username)
+        user_count = user_list.count(username) + 1
         user_list.append(username)
         message = request.form['message']
+        message = my_filter.censor(message)
         if username == '':
             message_list.append(f'名無しさん:{message}')
             return redirect("/")
